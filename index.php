057a1cf0f11c80b82ebc6660b1deb703d96a4c0f
1213terry <1213terry@naver.com>
2020-06-09 07:41:44 +0100

    header fix

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <title>Hishop</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css"/>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/base/style.css">
 
     <script src="/js/includeHTML.js"></script>
 </head>
