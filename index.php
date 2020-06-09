6b58ddf890709222eab9f60ce761e2bffba58975
Yeonwoo Lim <1213terry@naver.com>
2020-06-09 15:40:19 +0900

    header fix

--- index.php
+++ index.php
@@ -2,19 +2,16 @@
 <html>
 
 <head>
-<meta charset="UTF-8">
-<title>분리된 HTML</title>
-<script src="/js/includeHTML.js"></script>
+    <meta charset="UTF-8">
+    <title>Hishop</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css"/>
+    <link rel="stylesheet" href="style.css">
+
+    <script src="/js/includeHTML.js"></script>
 </head>
 
 <body>
-<header include-html="/base/header.html"></header>
-
-<h1>My first PHP page</h1>
-
-<?php
-echo "Hello World!";
-?>
+    <header include-html="/base/header.html"></header>
 
 </body>
 
