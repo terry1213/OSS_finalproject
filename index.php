25d97acc970c1c5b24fdac9f352ba432ad023e85
1213terry <1213terry@naver.com>
2020-06-09 07:09:43 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOVTYPE html>
+<html>
+<header>
+</header>
+<body>
+<header include-html="base/header.html"></header>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "Hello World!";
+?>
+
+</body>
+</html>
