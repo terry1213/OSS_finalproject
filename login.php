8b26e1ffe6dca0c09a17d43844c9aaaf990761da
Yeonwoo Lim <1213terry@naver.com>
2020-06-10 19:57:52 +0900

    login fix

--- login.php
+++ login.php
@@ -11,22 +11,24 @@
         include ("base/header.php");
     ?>
 
-    <div class ="container">
-       <div class="col-lg-4" style="margin:auto">
-          <div class="jumbotron" style="padding-top:20px;">
-             <form method='post' action='login_ok.php'>
-                <h2 style="text-align:center;">Login</h2>
-                <div class="form-group">
-                   <input class="form-control" type='text' name='user_id'/>
-                </div>
-                <div class="form-group">
-                   <input class="form-control" type='password' name='user_pw'/>
-                </div>
-                <input type='submit' class="btn btn-dark form-control" value='로그인'/>
-             </form>
-          </div>
-       </div>
-    </div>
+    <article>
+        <div class ="container">
+           <div class="col-lg-4" style="margin:auto">
+              <div class="jumbotron" style="padding-top:20px;">
+                 <form method='post' action='login_ok.php'>
+                    <h2 style="text-align:center;">Login</h2>
+                    <div class="form-group">
+                       <input class="form-control" type='text' name='user_id'/>
+                    </div>
+                    <div class="form-group">
+                       <input class="form-control" type='password' name='user_pw'/>
+                    </div>
+                    <input type='submit' class="btn btn-dark form-control" value='로그인'/>
+                 </form>
+              </div>
+           </div>
+        </div>
+    </article>
 
     <?php
         include ("base/footer.php");
