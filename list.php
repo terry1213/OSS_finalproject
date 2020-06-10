a4bc24efa839fcc2a30340453044e9d3ae153543
Yeonwoo Lim <1213terry@naver.com>
2020-06-10 14:29:02 +0900

    list fix

--- list.php
+++ list.php
@@ -18,23 +18,41 @@
     <header include-html="/base/header.html"></header>
     
     <table>
+        <thead>
+            <tr>
+                <th>
+                    NO.
+                </th>
+                <th>
+                    물품
+                </th>
+                <th>
+                    위치
+                </th>
+                <th>
+                    날짜
+                </th>
+                <th>
+                    상태
+                </th>
+            </tr>
+        </thead>
     <?php
         $conn = mysqli_connect(
           'localhost',
           'root',
           '1213',
           'histhing');
-        $sql = "SELECT * FROM Item";
+        $sql = "SELECT writer_id, name, locate, date, state FROM Item";
         $result = mysqli_query($conn, $sql);
-        $row = mysqli_fetch_array($result);
         while($row = mysqli_fetch_array($result)) {
-            echo '<h2>'.$row['writer_id'].'</h2>';
-            echo '<h2>'.$row['phone'].'</h2>';
-            echo '<h2>'.$row['locate'].'</h2>';
-            echo '<h2>'.$row['date'].'</h2>';
-            echo '<h2>'.$row['state'].'</h2>';
-            echo '<h2>'.$row['quiz'].'</h2>';
-            echo '<h2>'.$row['answer'].'</h2>';
+            echo '<tr>';
+            echo '<td>'.$row['writer_id'].'</td>';
+            echo '<td>'.$row['name'].'</td>';
+            echo '<td>'.$row['locate'].'</td>';
+            echo '<td>'.$row['date'].'</td>';
+            echo '<td>'.$row['state'].'</td>';
+            echo '</tr>';
         }
     ?>
     </table>
