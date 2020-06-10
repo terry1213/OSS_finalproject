e140f2bfe5c2aa96364c68d3e609c56f5319a18f
Yeonwoo Lim <1213terry@naver.com>
2020-06-10 23:13:29 +0900

    icon(fontawesome) add

--- base/head.php
+++ base/head.php
@@ -3,7 +3,7 @@
     <title>Hishop</title>
     <link rel="shortcut icon" href="image/favicon.ico">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="/css/style.css">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
