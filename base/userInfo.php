ab5a11b9fc5c1b66b4ff587bd4d1d0be1e6b4b08
Yeonwoo Lim <1213terry@naver.com>
2020-06-10 21:47:03 +0900

    nav -> aside

--- base/userInfo.php
+++ base/userInfo.php
@@ -1,4 +1,4 @@
-<nav class="user-info">
+<aside class="user-info">
     <?php
         session_start();
         if(!isset($_SESSION['user_id']) || !isset($_SESSION['user_name'])) {
@@ -10,4 +10,4 @@
         echo "<button class='btn btn-info' onClick=\"location.href='/logout.php'\">로그아웃</button>";
         echo "<a>안녕하세요. $user_name($user_id)님</a>";
     ?>
-</nav>
+</aside>
