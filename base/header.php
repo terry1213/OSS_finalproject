bfce3bf0ebd6ec5933794b8e1e76cf865ebfcdad
Yeonwoo Lim <1213terry@naver.com>
2020-06-10 17:56:33 +0900

    logo add

--- base/header.php
+++ base/header.php
@@ -1,21 +1,22 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" onClick="location.href='/'">Histhing</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item active" onClick="location.href='/'">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item" onClick="location.href='/list.php'">
-            <a class="nav-link">List</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link">My Page</a>
-          </li>
-        </ul>
-      </div>
+        <img src="base/logo.png" alt="logo"/>
+        <a class="navbar-brand" onClick="location.href='/'">Histhing</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active" onClick="location.href='/'">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item" onClick="location.href='/list.php'">
+                    <a class="nav-link">List</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">My Page</a>
+                </li>
+            </ul>
+        </div>
     </nav>
 </header>
