0e0f4e782c8020efb49f29a8b91f159bc4ce976d
Yeonwoo Lim <1213terry@naver.com>
2020-06-10 20:05:36 +0900

    login test

--- login.php
+++ login.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!DOVTYPE html>
 <html>
 
 <?php
@@ -6,13 +6,56 @@
 ?>
 
 <body>
-
     <?php
         include ("base/header.php");
     ?>
+    <?php
+        include ("base/userInfo.php");
+    ?>
 
     <article>
-
+        <table class="table table-striped item-list">
+            <thead>
+                <tr>
+                    <th scope="col">
+                        NO.
+                    </th>
+                    <th scope="col">
+                        물품
+                    </th>
+                    <th scope="col">
+                        위치
+                    </th>
+                    <th scope="col">
+                        날짜
+                    </th>
+                    <th scope="col">
+                        상태
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php
+                $conn = mysqli_connect(
+                  'localhost',
+                  'root',
+                  '1213',
+                  'histhing');
+                $sql = "SELECT writer_id, name, locate, date, state FROM Item";
+                $result = mysqli_query($conn, $sql);
+                $count = 0;
+                while($row = mysqli_fetch_array($result)) {
+                    echo '<tr>';
+                    echo '<td scope="row">'.++$count.'</td>';
+                    echo '<td>'.$row['name'].'</td>';
+                    echo '<td>'.$row['locate'].'</td>';
+                    echo '<td>'.$row['date'].'</td>';
+                    echo '<td>'.$row['state'].'</td>';
+                    echo '</tr>';
+                }
+            ?>
+            </tbody>
+        </table>
     </article>
 
     <?php
