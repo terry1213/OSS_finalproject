fa5b441aed89804d3b946fde7f32911b30a53c3d
Yeonwoo Lim <1213terry@naver.com>
2020-06-10 16:37:50 +0900

    item-list table fix

--- list.php
+++ list.php
@@ -29,49 +29,50 @@
             echo "<a>안녕하세요. $user_name($user_id)님</a>";
         ?>
     </nav>
-
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th scope="col">
-                    NO.
-                </th>
-                <th scope="col">
-                    물품
-                </th>
-                <th scope="col">
-                    위치
-                </th>
-                <th scope="col">
-                    날짜
-                </th>
-                <th scope="col">
-                    상태
-                </th>
-            </tr>
-	</thead>
-	<tbody>
-        <?php
-            $conn = mysqli_connect(
-              'localhost',
-              'root',
-              '1213',
-              'histhing');
-            $sql = "SELECT writer_id, name, locate, date, state FROM Item";
-            $result = mysqli_query($conn, $sql);
-            $count = 0;
-            while($row = mysqli_fetch_array($result)) {
-                echo '<tr>';
-                echo '<td scope="row">'.++$count.'</td>';
-                echo '<td>'.$row['name'].'</td>';
-                echo '<td>'.$row['locate'].'</td>';
-                echo '<td>'.$row['date'].'</td>';
-                echo '<td>'.$row['state'].'</td>';
-                echo '</tr>';
-            }
-        ?>
-	</tbody>
-    </table>
+    <article>
+        <table class="table table-striped item-list">
+            <thead>
+                <tr>
+                    <th scope="col">
+                        NO.
+                    </th>
+                    <th scope="col">
+                        물품
+                    </th>
+                    <th scope="col">
+                        위치
+                    </th>
+                    <th scope="col">
+                        날짜
+                    </th>
+                    <th scope="col">
+                        상태
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php
+                $conn = mysqli_connect(
+                  'localhost',
+                  'root',
+                  '1213',
+                  'histhing');
+                $sql = "SELECT writer_id, name, locate, date, state FROM Item";
+                $result = mysqli_query($conn, $sql);
+                $count = 0;
+                while($row = mysqli_fetch_array($result)) {
+                    echo '<tr>';
+                    echo '<td scope="row">'.++$count.'</td>';
+                    echo '<td>'.$row['name'].'</td>';
+                    echo '<td>'.$row['locate'].'</td>';
+                    echo '<td>'.$row['date'].'</td>';
+                    echo '<td>'.$row['state'].'</td>';
+                    echo '</tr>';
+                }
+            ?>
+            </tbody>
+        </table>
+    </article>
 </body>
 
 <script>
