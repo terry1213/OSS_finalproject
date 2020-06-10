42113b2cb767ab543151c3a3f11087fb931fe972
Yeonwoo Lim <1213terry@naver.com>
2020-06-10 18:50:40 +0900

    header navbar style fix

--- base/header.php
+++ base/header.php
@@ -1,5 +1,5 @@
 <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light">
         <img src="base/logo.png" alt="logo" width="60" height="60"/>
         <a class="navbar-brand" onClick="location.href='/'">Histhing</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
