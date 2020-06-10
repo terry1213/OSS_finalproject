10617f2aa14b6019aeae30e4eaa4d644c8d0e2b0
Yeonwoo Lim <1213terry@naver.com>
2020-06-10 17:38:31 +0900

    icon add

--- base/head.php
+++ base/head.php
@@ -1,6 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Hishop</title>
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css"/>
     <link rel="stylesheet" href="/base/style.css">
