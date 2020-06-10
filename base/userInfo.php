1545ff497757bb0dcc120d53b9cc783fcd479b6a
Yeonwoo Lim <1213terry@naver.com>
2020-06-10 23:00:39 +0900

    userInfo fix

--- base/userInfo.php
+++ base/userInfo.php
@@ -7,7 +7,7 @@
         }
         $user_id = $_SESSION['user_id'];
         $user_name = $_SESSION['user_name'];
-        echo "<div class=\"profile-container container\">";
+        echo "<div class=\"container profile-container\">";
         echo "<a>안녕하세요.<br>$user_name($user_id)님</a>";
         echo "<button class='btn btn-info' onClick=\"location.href='/logout.php'\">로그아웃</button>";
         echo "</div>"
