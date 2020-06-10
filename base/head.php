ad41d3222394b38c44c9737f870c4e155bd7dbc9
Yeonwoo Lim <1213terry@naver.com>
2020-06-10 20:16:26 +0900

    css path fix

--- base/head.php
+++ base/head.php
@@ -4,7 +4,7 @@
     <link rel="shortcut icon" href="image/favicon.ico">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css"/>
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/style.css?v=4">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
