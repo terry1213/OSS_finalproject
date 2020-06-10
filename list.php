e61f71b050b7d92853ce5c3ad632dda058252dad
Yeonwoo Lim <1213terry@naver.com>
2020-06-10 16:18:07 +0900

    list fix

--- list.php
+++ list.php
@@ -16,17 +16,19 @@
 
 <body>
     <header include-html="/base/header.html"></header>
-    <?php
-        session_start();
-        if(!isset($_SESSION['user_id']) || !isset($_SESSION['user_name'])) {
-            echo "<meta http-equiv='refresh' content='0;url=login.php'>";
-            exit;
-        }
-        $user_id = $_SESSION['user_id'];
-        $user_name = $_SESSION['user_name'];
-        echo "<p>안녕하세요. $user_name($user_id)님</p>";
-        echo "<p><a href='logout.php'>로그아웃</a></p>";
-    ?>
+    <nav>
+        <?php
+            session_start();
+            if(!isset($_SESSION['user_id']) || !isset($_SESSION['user_name'])) {
+                echo "<meta http-equiv='refresh' content='0;url=login.php'>";
+                exit;
+            }
+            $user_id = $_SESSION['user_id'];
+            $user_name = $_SESSION['user_name'];
+            echo "<p>안녕하세요. $user_name($user_id)님</p>";
+            echo "<button class='btn btn-info' href='logout.php'>로그아웃</button>";
+        ?>
+    </nav>
 
     <table class="table table-striped">
         <thead>
