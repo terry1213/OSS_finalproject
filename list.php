e0e0ed5c6e7f43fc32a2e5b49587d12393488915
Yeonwoo Lim <1213terry@naver.com>
2020-06-10 14:37:37 +0900

    list fix

--- list.php
+++ list.php
@@ -17,45 +17,46 @@
 <body>
     <header include-html="/base/header.html"></header>
     
-    <table>
+    <table class="table table-striped">
         <thead>
             <tr>
-                <th>
+                <th scope="col">
                     NO.
                 </th>
-                <th>
+                <th scope="col">
                     물품
                 </th>
-                <th>
+                <th scope="col">
                     위치
                 </th>
-                <th>
+                <th scope="col">
                     날짜
                 </th>
-                <th>
+                <th scope="col">
                     상태
                 </th>
             </tr>
 	</thead>
 	<tbody>
-    <?php
-        $conn = mysqli_connect(
-          'localhost',
-          'root',
-          '1213',
-          'histhing');
-        $sql = "SELECT writer_id, name, locate, date, state FROM Item";
-        $result = mysqli_query($conn, $sql);
-        while($row = mysqli_fetch_array($result)) {
-            echo '<tr>';
-            echo '<td>'.$row['writer_id'].'</td>';
-            echo '<td>'.$row['name'].'</td>';
-            echo '<td>'.$row['locate'].'</td>';
-            echo '<td>'.$row['date'].'</td>';
-            echo '<td>'.$row['state'].'</td>';
-            echo '</tr>';
-        }
-	?>
+        <?php
+            $conn = mysqli_connect(
+              'localhost',
+              'root',
+              '1213',
+              'histhing');
+            $sql = "SELECT writer_id, name, locate, date, state FROM Item";
+            $result = mysqli_query($conn, $sql);
+            $count = 0;
+            while($row = mysqli_fetch_array($result)) {
+                echo '<tr>';
+                echo '<td scope="row">'.++count.'</td>';
+                echo '<td>'.$row['name'].'</td>';
+                echo '<td>'.$row['locate'].'</td>';
+                echo '<td>'.$row['date'].'</td>';
+                echo '<td>'.$row['state'].'</td>';
+                echo '</tr>';
+            }
+        ?>
 	</tbody>
     </table>
 </body>
