c99e774d169e56806e97be06db29193d306c1d7a
Yeonwoo Lim <1213terry@naver.com>
2020-06-10 20:51:01 +0900

    upload.php create

---
+++ upload.php
@@ -0,0 +1,25 @@
+<!DOVTYPE html>
+<html>
+
+<?php
+    include ("base/head.php");
+?>
+
+<body>
+    
+    <?php
+        include ("base/header.php");
+    ?>
+    <?php
+        include ("base/userInfo.php");
+    ?>
+    
+    <article>
+    </article>
+    
+    <?php
+        include ("base/footer.php");
+    ?>
+</body>
+
+</html>
