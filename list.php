8a18160f755acfac2a14673dcbb013548c0ac848
Yeonwoo Lim <1213terry@naver.com>
2020-06-10 16:07:37 +0900

    list fix

--- list.php
+++ list.php
@@ -15,6 +15,7 @@
 </head>
 
 <body>
+    <header include-html="/base/header.html"></header>
     <?php
         session_start();
         if(!isset($_SESSION['user_id']) || !isset($_SESSION['user_name'])) {
@@ -26,8 +27,7 @@
         echo "<p>안녕하세요. $user_name($user_id)님</p>";
         echo "<p><a href='logout.php'>로그아웃</a></p>";
     ?>
-    <header include-html="/base/header.php"></header>
-    
+
     <table class="table table-striped">
         <thead>
             <tr>
