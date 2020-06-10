bad266b7153e3529928f441164d9cdd589b823ba
Yeonwoo Lim <1213terry@naver.com>
2020-06-10 19:09:54 +0900

    source path fix

--- base/head.php
+++ base/head.php
@@ -1,10 +1,10 @@
 <head>
     <meta charset="UTF-8">
     <title>Hishop</title>
-    <link rel="shortcut icon" href="http://192.168.35.17/base/favicon.ico">
+    <link rel="shortcut icon" href="image/favicon.ico">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css"/>
-    <link rel="stylesheet" href="/base/style.css">
+    <link rel="stylesheet" href="/css/style.css">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
