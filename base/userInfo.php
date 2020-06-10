8b553186996e8e5b06716f25d1ecbfde54cb1ca0
Yeonwoo Lim <1213terry@naver.com>
2020-06-10 23:13:36 +0900

    userInfo fix

--- base/userInfo.php
+++ base/userInfo.php
@@ -9,6 +9,20 @@
         $user_name = $_SESSION['user_name'];
         echo "<div class=\"profile-container container\">";
         echo "<a>안녕하세요.<br>$user_name($user_id)님</a>";
+        
+        echo "<table class='table'>";
+        echo "<tbody>";
+        echo "<tr>";
+        echo "<th scope='row'>아이디</th>";
+        echo "<td>$user_id</td>";
+        echo "</tr>";
+        echo "<tr>";
+        echo "<th scope='row'><i class='fa fa-user' aria-hidden='true'></i>이름</th>";
+        echo "<td>$user_name</td>";
+        echo "</tr>";
+        echo "</tbody>";
+        echo "</table>";
+        
         echo "<button class='btn btn-info' onClick=\"location.href='/logout.php'\">로그아웃</button>";
         echo "</div>"
     ?>
