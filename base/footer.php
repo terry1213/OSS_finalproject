630b5512255cff6a4f6f06a527b92fa02ed0087c
Yeonwoo Lim <1213terry@naver.com>
2020-06-10 18:45:27 +0900

    footer fix

--- base/footer.php
+++ base/footer.php
@@ -7,43 +7,23 @@
             </div>
             <hr class="clearfix w-100 d-md-none pb-3">
             <div class="col-md-3 mb-md-0 mb-3">
-                <h5 class="text-uppercase">Links</h5>
+                <h5>LINKS</h5>
                 <ul class="list-unstyled">
                     <li>
-                        <a href="#!">Link 1</a>
+                        <a href="/">Home</a>
                     </li>
                     <li>
-                        <a href="#!">Link 2</a>
+                        <a href="/list.php">List</a>
                     </li>
                     <li>
-                        <a href="#!">Link 3</a>
-                    </li>
-                    <li>
-                        <a href="#!">Link 4</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-md-3 mb-md-0 mb-3">
-                <h5 class="text-uppercase">Links</h5>
-                <ul class="list-unstyled">
-                    <li>
-                        <a href="#!">Link 1</a>
-                    </li>
-                    <li>
-                        <a href="#!">Link 2</a>
-                    </li>
-                    <li>
-                        <a href="#!">Link 3</a>
-                    </li>
-                    <li>
-                        <a href="#!">Link 4</a>
+                        <a href="/MyPage.php">My Page</a>
                     </li>
                 </ul>
             </div>
         </div>
     </div>
 
-    <div class="footer-copyright text-center py-3">© 2020 Copyright:
-        <a href="https://mdbootstrap.com/"> Yeonwoo Lim</a>
+    <div class="copyright text-center py-3">© 2020 Copyright:
+        <a href="/"> Yeonwoo Lim</a>
     </div>
 </footer>
