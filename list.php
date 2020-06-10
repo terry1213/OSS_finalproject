c0b984ac5423ee243a47c39e19464a13dcbdd199
Yeonwoo Lim <1213terry@naver.com>
2020-06-10 13:49:16 +0900

    list fix

--- list.php
+++ list.php
@@ -16,6 +16,28 @@
 
 <body>
     <header include-html="/base/header.html"></header>
+    
+    <table>
+    <?php
+        $conn = mysqli_connect(
+          'localhost',
+          'root',
+          '1213',
+          'histhing');
+        $sql = "SELECT * FROM item";
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_array($result);
+        while($row = mysqli_fetch_array($result)) {
+            echo '<h2>'.$row['writer_id'].'</h2>';
+            echo '<h2>'.$row['phone'].'</h2>';
+            echo '<h2>'.$row['locate'].'</h2>';
+            echo '<h2>'.$row['date'].'</h2>';
+            echo '<h2>'.$row['state'].'</h2>';
+            echo '<h2>'.$row['quiz'].'</h2>';
+            echo '<h2>'.$row['answer'].'</h2>';
+        }
+    ?>
+    </table>
 </body>
 
 <script>
