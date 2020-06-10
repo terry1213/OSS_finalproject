63dd402454149d6691327e251ca2d6ec4955c04c
Yeonwoo Lim <1213terry@naver.com>
2020-06-10 17:42:14 +0900

    login fix

--- login.php
+++ login.php
@@ -1,21 +1,16 @@
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="UTF-8">
-<title>Hishop</title>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css"/>
-<link rel="stylesheet" href="/base/style.css">
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-<script src="/js/includeHTML.js"></script>
-
-</head>
+<?php
+    include ("base/head.php");
+?>
 
 <body>
-    <header include-html="/base/header.html"></header>
+
+    <?php
+        include ("base/header.php");
+    ?>
+
     <div class ="container">
        <div class="col-lg-4" style="margin:auto">
           <div class="jumbotron" style="padding-top:20px;">
