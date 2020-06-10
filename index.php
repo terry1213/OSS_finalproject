490ea922028f59fcadf82c16978a24bf3d600184
Yeonwoo Lim <1213terry@naver.com>
2020-06-10 16:04:54 +0900

    index fix

--- index.php
+++ index.php
@@ -15,7 +15,6 @@
 </head>
 
 <body>
-    <header include-html="/base/header.html"></header>
     <?php
         session_start();
         if(!isset($_SESSION['user_id']) || !isset($_SESSION['user_name'])) {
@@ -27,6 +26,7 @@
         echo "<p>안녕하세요. $user_name($user_id)님</p>";
         echo "<p><a href='logout.php'>로그아웃</a></p>";
     ?>
+    <header include-html="/base/header.php"></header>
 </body>
 
 <script>
