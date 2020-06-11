485be613fa1eb75f08243229905d0ccdeeaa27ed
Yeonwoo Lim <1213terry@naver.com>
2020-06-11 21:02:17 +0900

    user db table  apply

--- login_ok.php
+++ login_ok.php
@@ -1,22 +1,22 @@
-
 <?php
-if(!isset($_POST['user_id']) || !isset($_POST['user_pw'])) exit;
-$user_id = $_POST['user_id'];
-$user_pw = $_POST['user_pw'];
-$members = [
-        '21500602'=>['pw'=>'1213', 'name'=>'임연우'],
-        '21900613'=>['pw'=>'6060', 'name'=>'임연규']
-];
-if(!isset($members[$user_id])) {
+    if(!isset($_POST['user_id']) || !isset($_POST['user_pw'])) exit;
+    $user_id = $_POST['user_id'];
+    $user_pw = $_POST['user_pw'];
+    $conn = mysqli_connect(
+        'localhost',
+        'root',
+        '1213',
+        'histhing');
+    $sql = "SELECT * FROM User WHERE user_id='".$user_id."' AND password='".$user_pw."'";
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) < 1) {
         echo "<script>alert('아이디 또는 패스워드가 잘못되었습니다.');history.back();</script>";
         exit;
-}
-if($members[$user_id]['pw'] != $user_pw) {
-        echo "<script>alert('아이디 또는 패스워드가 잘못되었습니다.');history.back();</script>";
-        exit;
-}
-session_start();
-$_SESSION['user_id'] = $user_id;
-$_SESSION['user_name'] = $members[$user_id]['name'];
+    }
+    
+    $row = mysqli_fetch_array($result);
+    session_start();
+    $_SESSION['user_id'] = $row['user_id'];
+    $_SESSION['user_name'] = $row['name'];
 ?>
 <meta http-equiv='refresh' content='0;url=/'>
