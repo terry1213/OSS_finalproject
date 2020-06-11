c7d1f1a11cbf4334b326c3eb1b20d42c0492b0d2
Yeonwoo Lim <1213terry@naver.com>
2020-06-11 22:42:36 +0900

    placeholder & signup button add

--- login.php
+++ login.php
@@ -16,12 +16,13 @@
                 <form method='post' action='login_ok.php'>
                     <h2 class="text-center">Login</h2>
                     <div class="form-group">
-                        <input class="form-control" type='text' name='user_id'/>
+                        <input class="form-control" type='text' name='user_id' placeholder="ID"/>
                     </div>
                     <div class="form-group">
-                        <input class="form-control" type='password' name='user_pw'/>
+                        <input class="form-control" type='password' name='user_pw' placeholder="Password"/>
                     </div>
                     <input type='submit' class="btn btn-dark form-control" value='로그인'/>
+                    <input type='button' class="btn btn-dark form-control" value='회원가입' onClick="location.href='/signUp.php'"/>
                 </form>
             </div>
         </div>
