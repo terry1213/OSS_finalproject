00e9b562295bdd7a1c65a9cd1728d03b3fe0acbc
Yeonwoo Lim <1213terry@naver.com>
2020-06-12 20:50:39 +0900

    image input add

--- upload.php
+++ upload.php
@@ -16,7 +16,7 @@
     
     <div class="wrap">
         <article>
-            <form class="upload-form" name="upload-form" id="upload-form" role="form" method="post" action="\uploadAction.php">
+            <form class="upload-form" name="upload-form" id="upload-form" role="form" method="post" action="\uploadAction.php" enctype="multipart/form-data">
                 <div class="form-group">
                     <label for="name">물품</label>
                     <input type="text" class="form-control" name="name" id="name" placeholder="물품명을 입력해 주세요." required>
@@ -27,15 +27,31 @@
                 </div>
                 <div class="form-group">
                     <label for="phone">전화번호</label>
-                    <input type="tel" class="form-control" name="phone" id="phone" pattern="[0-9]{3}-[0-9]{4}-[0-9]{4}|[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="연락 받을 번호를 입력해 주세요." required>
+                    <?php
+                        $user_id = $_SESSION['user_id'];
+                        $conn = mysqli_connect(
+                          'localhost',
+                          'root',
+                          '1213',
+                          'histhing');
+                        $sql = "SELECT phone FROM User WHERE user_id='".$user_id."'";
+                        $result = mysqli_query($conn, $sql);
+                        $row = mysqli_fetch_array($result);
+                        echo "<input type='tel' class='form-control' name='phone' id='phone' pattern='[0-9]{3}-[0-9]{4}-[0-9]{4}|[0-9]{3}-[0-9]{3}-[0-9]{4}' placeholder='연락 받을 번호를 입력해 주세요.' value='".$row['phone']."' required>"
+                    ?>
                 </div>
                 <div class="form-group">
                     <label for="detail">상세 설명</label>
                     <textarea class="form-control" rows="5" name="detail" id="detail" placeholder="상세한 설명을 입력해 주세요."></textarea>
                 </div>
+                <p><strong>물품 사진</strong></p>
+                <div class="custom-file form-group">
+                    <label class="custom-file-label" for="image">물품 사진을 선택해주세요.</label>
+                    <input type="file" class="custom-file-input" name="image" id="image">
+                </div>
                 <div class="form-check">
                     <input type="checkbox" class="form-check-input" name="quizCheck" id="quizCheck" onclick="checkEvent();">
-<label class="form-check-label" for="quizCheck">질문 생성(질문 생성 시 해당 질문을 맞춰야만 작성자의 전화번호가 보입니다.)</label>
+                    <label class="form-check-label" for="quizCheck">질문 생성(질문 생성 시 해당 질문을 맞춰야만 작성자의 전화번호가 보입니다.)</label>
                 </div>
                 <div class="form-group hidden-form-group">
                     <label for="quiz">질문</label>
@@ -45,7 +61,7 @@
                     <label for="answer">정답</label>
                     <input type="text" class="form-control" name="answer" id="answer" placeholder="질문에 대한 정답을 입력해 주세요.">
                 </div>
-                <button class="btn btn-info" type="submit" >게시하기</button>
+                <button class="btn btn-info" type="submit" name="submit">게시하기</button>
             </form>
         </article>
         
@@ -112,5 +128,9 @@
         }
     }
 
+    $(".custom-file-input").on("change", function() {
+      var fileName = $(this).val().split("\\").pop();
+      $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+    });
 </script>
 </html>
--- uploadAction.php
+++ uploadAction.php
@@ -6,13 +6,58 @@
     }
     $user_id = $_SESSION['user_id'];
     $user_name = $_SESSION['user_name'];
+    
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["image"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+    if(isset($_POST["submit"])) {
+        $check = getimagesize($_FILES["image"]["tmp_name"]);
+        if($check == false) {
+            echo "<script>alert('이미지 파일이 아닙니다.');history.back();</script>";
+        }
+    }
+
+    $FileType = substr(strrchr(basename($_FILES["image"]["name"]), "."), 1);
+    $FileName = substr(basename($_FILES["image"]["name"]), 0, strlen(basename($_FILES["image"]["name"])) - strlen($FileType) - 1);
+
+    $tmp = "$FileName.$FileType";
+    $count = 0;
+    while(file_exists($target_dir.$tmp)){
+        $count++;
+        $tmp = $FileName."_".$count.".".$FileType;
+    }
+    $target_file = $target_dir . $tmp;
+    
+
+    while (file_exists($target_file)) {
+        $_FILES["image"]["name"] += "1";
+    }
+
+    if ($_FILES["image"]["size"] > 3000000) {
+        echo "<script>alert('이미지의 용량이 3MB보다 큽니다.');history.back();</script>";
+    }
+
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif" ) {
+        echo "<script>alert('JPG, JPEG, PNG, GIF 파일만 업로드할 수 있습니다.');history.back();</script>";
+    }
+    
+    if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file) != 1) {
+        echo "<script>alert('이미지 업로드가 실패했습니다.');history.back();</script>";
+    }
+
+
+
+
     $conn = mysqli_connect(
       'localhost',
       'root',
       '1213',
       'histhing');
-    $sql = "INSERT INTO Item( writer_id, phone, locate, date, state, quiz, answer, name, detail) VALUES ('".$user_id."', '".$_POST['phone']."', '".$_POST['locate']."', now(), 'lost', '".$_POST['quiz']."', '".$_POST['answer']."', '".$_POST['name']."', '".$_POST['detail']."')";
-    
+    $sql = "INSERT INTO Item( writer_id, phone, locate, date, state, quiz, answer, name, detail, image_path) VALUES ('".$user_id."', '".$_POST['phone']."', '".$_POST['locate']."', now(), 'lost', '".$_POST['quiz']."', '".$_POST['answer']."', '".$_POST['name']."', '".$_POST['detail']."', '".$target_file."')";
+
     $result = mysqli_query($conn, $sql);
     if($result === false){
         echo mysqli_error($conn);
