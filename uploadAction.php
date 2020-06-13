ba79f64806f54845f8ab251b2ba56c57b007bce1
Yeonwoo Lim <1213terry@naver.com>
2020-06-13 21:56:04 +0900

    test

--- uploadAction.php
+++ uploadAction.php
@@ -45,7 +45,9 @@
         }
         
         if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file) != 1) {
-            echo "<script>alert('이미지 업로드가 실패했습니다.');history.back();</script>";
+            echo $target_file;
+            echo $_FILES["image"]["tmp_name"];
+//            echo "<script>alert('이미지 업로드가 실패했습니다.');history.back();</script>";
             exit;
         }
     }
