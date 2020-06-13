5d4e790fdacbf7dc8b11a8e51182b21b5b25a3df
Yeonwoo Lim <1213terry@naver.com>
2020-06-13 12:33:40 +0900

    signup error fix

--- signUpAction.php
+++ signUpAction.php
@@ -5,7 +5,7 @@
       '1213',
       'histhing');
     
-    $sql = "SELECT EXISTS(SELECT * FROM User WHERE user_id='".$_POST['user_id']."')";
+    $sql = "SELECT * FROM User WHERE user_id='".$_POST['user_id']."'";
     $result = mysqli_query($conn, $sql);
     if(mysqli_num_rows($result) == 1){
         echo "<script>alert('이미 존재하는 아이디입니다.');history.back();</script>";
